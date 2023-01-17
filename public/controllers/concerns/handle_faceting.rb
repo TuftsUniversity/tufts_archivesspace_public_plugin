e8fee01ba9d4de999ef1b43346f274ae2f9a2cf4
HSteeleLTS <44376066+HSteeleLTS@users.noreply.github.com>
2023-01-17 17:17:56 -0500

    adjust models, controllers, and concerns for 3.3.1

--- public/controllers/concerns/handle_faceting.rb
+++ public/controllers/concerns/handle_faceting.rb
@@ -9,27 +9,27 @@ module HandleFaceting
     criteria[:page_size] = 1
     criteria['facet[]'] = facets_array
     criteria['facet.mincount'] = 1 if !include_zero
-    data =  archivesspace.search(query, 1, criteria) || {}
+    data = archivesspace.search(query, 1, criteria) || {}
     faceting = {}
     if !data['facets'].blank? && !data['facets']['facet_fields'].blank?
       faceting = data['facets']['facet_fields']
     end
-   end
+  end
 
 
   # strip out: facets with counts less than input minimum or equal to the total hits, facets of form "ead/ arch*"
   # returns a hash with the text of the facet as the key, count as the value
-  def strip_facets(facets_array, min, total_hits = nil)
-    facets = {}
-    facets_array.each_slice(2) do |t, ct|
-      next if ct < min
-      next if total_hits && ct == total_hits
-      next if t.start_with?("ead/ archdesc/ ")
-      facets[t] = ct
+  def strip_facets(facets_array, min, type, total_hits = nil)
+    facets = []
+    facets_array.each_slice(2) do |facet_key, facet_count|
+      facets << Facet.new(type, facet_key, facet_count)
     end
+    facets.reject! { |f| f.count < min || f.count == total_hits }
+    facets.sort_by! { |f| f.label } if AppConfig[:pui_display_facets_alpha]
     facets
   end
-   # bury the mess! 
+
+   # bury the mess!
   def get_pretty_facet_value(k, v)
     pv = strip_mixed_content(v)
     if k == 'primary_type'
@@ -37,6 +37,9 @@ module HandleFaceting
     elsif k == 'langcode'
       # Lookup three-letter language codes
       pv = I18n.t("enumerations.language_iso639_2.#{v}", :default => v)
+    elsif k == 'type_enum_s'
+      # Lookup container type
+      pv = I18n.t("enumerations.container_type.#{v}", :default => v)
     elsif %w(repository used_within_published_repository).include?(k)
       repos = Repository.get_repos
       if repos[v].nil?
@@ -44,8 +47,6 @@ module HandleFaceting
       else
         pv = repos[v]['name'] || v
       end
-    elsif k == 'has_published_digital_objects'
-      pv = v ? 'Contains digital materials' : 'Does not contain digital materials'
     end
     pv
   end
